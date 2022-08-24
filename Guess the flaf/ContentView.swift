3259b1e67d3d846b88a8431318761315223eeca5
youcefhedibel <youyou_dz1@live.fr>
2022-08-24 10:49:34 +0100

    button & alerts

--- Guess the flaf/ContentView.swift
+++ Guess the flaf/ContentView.swift
@@ -8,39 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack{
+       
             
-            Button("button 1"){ }
-                .buttonStyle(.bordered)
-            Button("button 2", role: .destructive){ }
-                .buttonStyle(.bordered)
-            Button("button 3", role: .cancel){ }
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-            Button("button 4", role: .destructive){ }
-                .buttonStyle(.borderedProminent)
-            
-            Button {
-                print("you clicked me !")
-            } label: {
-                Text("tap me")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(.red)
+            Button("TEST"){
+                showingAlert = true
+            }.font(.system(size: 40))
+            .alert("this is alert message", isPresented: $showingAlert){
+                Button("OK"){
                     
+                }
             }
-            Button {
-                print("sam3Likooom")
-            } label: {
-                Label("edit", systemImage: "pencil")
-            }
+           
             
-    } .ignoresSafeArea()
+    
 }
-    func executeDelete (){
-        print("tatattaa")
-    }
+    
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
