3a01c922b18345b27c20aba72c7927865b234a62
youcefhedibel <youyou_dz1@live.fr>
2022-08-24 15:37:31 +0100

    upgrade design

--- Guess the flaf/ContentView.swift
+++ Guess the flaf/ContentView.swift
@@ -17,16 +17,22 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
-                
-            VStack(spacing : 30){
+        ZStack {
+            RadialGradient( stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
+                center: .top, startRadius: 200, endRadius: 700
+            )
+            VStack{
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold()).foregroundColor(.white)
+            VStack(spacing : 20){
                VStack{
-                        Text("Flag of").font(.subheadline.weight(.heavy))
-                   Text(countries[correctAnswer]).font(.largeTitle.weight(.semibold))
+                   Text("tap the Flag of").font(.subheadline.weight(.heavy)).foregroundStyle(.secondary)
+                   Text(countries[correctAnswer]).font(.largeTitle.weight(.semibold)).foregroundColor(.white)
                 }
                
-                    .foregroundColor(.white)
+                    
                     ForEach(0..<3){
                         number in Button {
                             flagTapped(number)
@@ -37,14 +43,19 @@ struct ContentView: View {
                         .shadow(radius: 10)
                 }
                     }
+            }   .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("Score : \(score) points").font(.title.bold()).foregroundColor(.white)
+                
             }
-            
         }
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
         } message: {
             Text("Your score is \(score)")
-            
         }
         .ignoresSafeArea()
 
@@ -71,5 +82,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
